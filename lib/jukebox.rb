a14903799ee1fff6b4e184ce23599bf2c6d34415
Khalid Tissire <ghettoguy7@gmail.com>
2020-01-10 09:01:39 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,17 +33,6 @@ def exit_jukebox
 end
 
 def run
-  songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
   while true do
     puts "Please enter a command:"
     cmd = gets.chomp.strip
