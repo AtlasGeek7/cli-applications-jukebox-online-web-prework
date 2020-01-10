b3d4339e2d1efb306e87f2ca073c38da855bb39d
Khalid Tissire <ghettoguy7@gmail.com>
2020-01-10 07:57:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,6 +32,11 @@ def play(arr)
   puts "Please enter a song name or number:"
   input = gets.strip
 end
+
+def play(arr)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+end
   
   
   
