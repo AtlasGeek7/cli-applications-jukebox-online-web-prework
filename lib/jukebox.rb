855357f1580a648afefefb5a2f078a6c23f00ea6
Khalid Tissire <ghettoguy7@gmail.com>
2020-01-10 08:40:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,7 +22,7 @@ def play(arr)
   puts "Please enter a song name or number:"
   input = gets.strip
   if arr.include?(input) || idx.include?(input.to_i)
-    input.length == 1 ? (puts "Playing #{arr[input-1]}") : (puts "Playing #{input}")
+    input.length == 1 ? (puts "Playing #{arr[input]}") : (puts "Playing #{input}")
   else
     puts "Invalid input, please try again"
   end
